b649d46608a4c252ff5d4b078d25f7252297e4d6
knochenmark <marktodd.hl@gmail.com>
2019-01-15 20:35:17 +0100

    Add basic test for pomodoro component

---
+++ src/Pomodoro.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import Pomodoro from './Pomodoro';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Pomodoro />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
