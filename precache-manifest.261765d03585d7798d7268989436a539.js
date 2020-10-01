79580c1682437a113e69acce138663e1e1ab6d55
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-01 19:56:37 +0000

    Deploy Knochenmark/pomodoro to github.com/Knochenmark/pomodoro.git:gh-pages

---
+++ precache-manifest.261765d03585d7798d7268989436a539.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c0569f22f452b5caad67860a77e5c3",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "4158fbcbbca14e8756ec",
+    "url": "/pomodoro/static/css/main.e97d408b.chunk.css"
+  },
+  {
+    "revision": "b5d5f068baca25f7437c",
+    "url": "/pomodoro/static/js/2.2bd5eb5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.2bd5eb5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4158fbcbbca14e8756ec",
+    "url": "/pomodoro/static/js/main.00a438cf.chunk.js"
+  },
+  {
+    "revision": "ee3590aa5d45d2dacf12",
+    "url": "/pomodoro/static/js/runtime-main.f3456f13.js"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/pomodoro/static/media/digital-7.mono.58045dab.ttf"
+  }
+]);
